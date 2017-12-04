c7f0e8ce7185f946bbe1f933489ba6b81c505897
lianbo2006 <513748889@qq.com>
2017-12-04 22:05:21 +0800

    test_index

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.template import Context, Template
+
 
 # Create your views here.
+def index(request):
+
+    context = {}
+    return render(request,'index.html',context)
